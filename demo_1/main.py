00459bf0ffe8fba3c6cd94fb05d0a9e8c5ea9f9a
iBvishal <bhardwaj.vishalnit@outlook.com>
2019-07-21 12:12:37 +0530

    training and testing

--- demo_1/main.py
+++ demo_1/main.py
@@ -23,7 +23,7 @@ mn_output = 10
 # defining the hyperparemeters
 dropout = 0.5       #threshold at which we eliminate some units at random, prevents overfitting
 batch_size = 128        #training examples used at each step
-mn_iterations = 1000        #how many times we go through training steps1
+mn_iterations = 2000        #how many times we go through training steps1
 learning_rate = 1e-4    #how much parameters will adjust at each learning process
 
 #defining tensors as placeholders
@@ -67,3 +67,26 @@ cross_entropy = tf.reduce_mean(
         labels=Y, logits=output_layer
         ))
 train_step = tf.compat.v1.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+# training and testing
+correct_pred = tf.equal(tf.argmax(output_layer, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# initialize a session to run the graph
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+# training on mini batches to speedup the training process
+for i in range(mn_iterations):
+    batch_x, batch_y = mnist.train.next_batch(batch_size)
+    sess.run(train_step, feed_dict={
+        X: batch_x, Y: batch_y, keep_prob: dropout
+    })
+    if i%100==0:
+        minibatch_loss, minibatch_accuracy = sess.run([cross_entropy, accuracy],feed_dict={X: batch_x, Y: batch_y, keep_prob: 1.0})
+        print("Iteration", str(i), "\t|Loss = ",str(minibatch_loss),"\t| Accuracy =",str(minibatch_accuracy))
+
+# running the session on test images
+test_accuracy = sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1.0})
+print("\nAccuracy on test set:", test_accuracy)
