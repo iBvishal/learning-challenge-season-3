f2a73cd939b86951dcd8d3c26d4c2a0c3ae2275f
iBvishal <bhardwaj.vishalnit@outlook.com>
2019-07-21 10:36:13 +0530

    defined network architecture

--- demo_1/main.py
+++ demo_1/main.py
@@ -10,3 +10,18 @@ mnist = input_data.read_data_sets("MSINT_data/",one_hot=True)
 mn_train = mnist.train.num_examples     #55,000
 mn_validation = mnist.validation.num_examples       #5000
 n_test = mnist.test.num_examples        #10,000
+
+# Defining model architecture
+mn_input = 784
+mn_hidden_1 = 512
+mn_hidden_2 = 256
+mn_hidden_3 = 128
+mn_hidden_4 = 64
+mn_hidden_5 = 32
+mn_output = 10
+
+# defining the hyperparemeters
+dropout = 0.5       #threshold at which we eliminate some units at random, prevents overfitting
+batch_size = 128        #training examples used at each step
+mn_iterations = 1000        #how many times we go through training steps1
+learning_rate = 1e-4    #how much parameters will adjust at each learning process
